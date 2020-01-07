08bd282cacc5a235ced809851396aae2c7fd5bbc
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-01-07 15:05:27 +0200

    add a simple test

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import "github.com/kataras/iris/v12"
 
 func main() {
-	app := iris.New()
-	app.Get("/", index)
+	app := newApp()
 
 	app.Run(iris.Addr(":8080"))
+}
+
+func newApp() *iris.Application {
+	app := iris.New()
+
+	app.Get("/", index)
+
+	return app
 }
 
 func index(ctx iris.Context) {

66429beb349f05d84d9e45097c29527bcb5dd69c
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-09-13 22:01:57 +0300

    iris v12.2.0-alpha

--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/iris-contrib/basic-template
 
-go 1.13
+go 1.15
 
 require (
-	github.com/kataras/iris/v12 v12.1.6
+	github.com/kataras/iris/v12 v12.2.0-alpha
 )
--- main.go
+++ main.go
@@ -5,12 +5,11 @@ import "github.com/kataras/iris/v12"
 func main() {
 	app := newApp()
 
-	app.Run(iris.Addr(":8080"))
+	app.Listen("8080")
 }
 
 func newApp() *iris.Application {
 	app := iris.New()
-
 	app.Get("/", index)
 
 	return app
